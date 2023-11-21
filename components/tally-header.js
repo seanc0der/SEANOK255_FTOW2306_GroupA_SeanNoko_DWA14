0849e08fd7a4d0438fdda229a3f60f56d05b7115
seanc0der <sean.noko@gmail.com>
2023-11-21 20:27:35 +0200

    Created a tally-header sub-component of the tally-app with its own styles and tagged template literal.

---
+++ components/tally-header.js
@@ -0,0 +1,66 @@
+import { LitElement, html, css } from "../libs/lit-html.js";
+
+class TallyHeader extends LitElement {
+	static styles = css`
+		.header-wrapper {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+
+		.header-wrapper h1 {
+			font-size: 2rem;
+		}
+
+		.menu-button::part(base) {
+			height: 4rem;
+			width: 5rem;
+			flex-direction: column;
+		}
+
+		.menu-items::part(label) {
+			font-size: 1.1rem;
+		}
+
+		.menu-icon {
+			display: block;
+			font-size: 2rem;
+			color: #a7a7ac;
+		}
+	`;
+
+	/**
+	 * @returns {any}
+	 */
+	render() {
+		return html`
+			<div class="header-wrapper">
+				<h1>Tally Count</h1>
+
+				<sl-dropdown>
+					<sl-button
+						class="menu-button"
+						variant="neutral"
+						outline="neutral"
+						slot="trigger"
+						size="large"
+					>
+						<sl-icon class="menu-icon" name="three-dots-vertical"></sl-icon>
+					</sl-button>
+
+					<sl-menu>
+						<sl-menu-item
+							class="menu-items"
+							data-reset-counter
+							value="reset-counter"
+						>
+							Reset Counter
+						</sl-menu-item>
+					</sl-menu>
+				</sl-dropdown>
+			</div>
+		`;
+	}
+}
+
+customElements.define("tally-header", TallyHeader);
