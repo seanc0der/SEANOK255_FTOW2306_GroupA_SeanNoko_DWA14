f0e86218fddb467e3b7c0e0d5667eb17274ce47b
seanc0der <sean.noko@gmail.com>
2023-11-22 22:48:47 +0200

    Finalized <tally-app> web component:
    - Imported 'tallyStateMachine' to manage state changes of the tally count based on user actions.
    - Implemented event-delegated custom events with a callback handler for executing received actions.
    - Included comprehensive JSDOC comments for enhanced code documentation.

--- components/tally-app.js
+++ components/tally-app.js
@@ -2,12 +2,22 @@
 
 import { LitElement, html, css } from "../libs/lit-html.js";
 
+import tallyStateMachine from "../modules/stateMachine/tally-state-machine.js";
+
 import "./tally-header.js";
 import "./tally-footer.js";
 
+/**
+ * `<tally-app>` is a custom web component designed for tally counting. It
+ * provides actions to increment, decrement, or reset the tally counter.
+ *
+ * @element tally-app
+ * @fires {CustomEvent} tallyResetAction - Fires a custom event when the tally counter is reset.
+ * @fires {CustomEvent} tallyButtonAction - Fires a custom event when the tally counter increment/decrement buttons are clicked.
+ */
 class TallyApp extends LitElement {
 	static properties = {
-		count: { type: Number, state: true },
+		count: { state: true },
 	};
 
 	static styles = css`
@@ -76,7 +86,7 @@ class TallyApp extends LitElement {
 
 	constructor() {
 		super();
-		this.count = 0;
+		this.count = tallyStateMachine.count;
 	}
 
 	/**
@@ -85,15 +95,40 @@ class TallyApp extends LitElement {
 	 */
 	render() {
 		return html`
-			<sl-card class="tally-container">
+			<sl-card
+				class="tally-container"
+				@tallyResetAction=${this.#alterTallyHandler}
+			>
 				<tally-header slot="header"></tally-header>
 				<p class="body">${this.count}</p>
-				<tally-footer slot="footer"></tally-footer>
+				<tally-footer
+					slot="footer"
+					@tallyButtonAction=${this.#alterTallyHandler}
+				></tally-footer>
 			</sl-card>
 		`;
+	}
+
+	/**
+	 * A customEvent handler which executes the passed action (`increment`,
+	 * `decrement`, or `resetCounter`) on the tally count via the
+	 * `tallyStateMachine` object's `runAction` method call.
+	 *
+	 *
+	 * @param {CustomEvent} event
+	 * @returns {void}
+	 */
+	#alterTallyHandler(event) {
+		const { action } = event.detail;
+
+		if (action === "resetCounter") {
+			tallyStateMachine.runAction(action);
+		} else {
+			tallyStateMachine.runAction(action);
+		}
+
+		this.count = tallyStateMachine.count;
 	}
 }
 
 customElements.define("tally-app", TallyApp);
-
-// /* Container Footer */
